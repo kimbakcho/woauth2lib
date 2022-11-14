59e5c1c4ff9c2ddcbf19f58b0ec82b7502c0a97a
beckho <vngkgk624@gmail.com>
2022-11-14 12:57:38 +0900

    리다이렉트 페이지 추가

---
+++ lib/RedirectLoginView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="mainBack">
+    <div id="mainDisPlay">
+      로그인 중
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {onMounted} from "vue";
+import {setupRedirect} from "@/oAuth2LibPlugin/oAuth2LibPlugin";
+import router from "@/router";
+onMounted(async ()=>{
+  try{
+    await setupRedirect()
+    await router.push({
+      path:"/"
+    })
+  }catch (e) {
+    alert("로그인 에러")
+    console.log("로그인 에러")
+  }
+})
+</script>
+
+<style scoped lang="scss">
+#mainBack {
+  background: url("http://10.20.10.114/common/loginback.png") no-repeat center fixed;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  #mainDisPlay {
+    font-size: 3rem;
+    font-weight: bold;
+  }
+}
+</style>
